962d6403ec4597c090bee7c6e8485ba7be54e518
hyerimmy <heyrim2010@naver.com>
2022-10-14 14:43:20 +0900

    회원도메인과 리포지토리 만들기

---
+++ src/main/java/hyerimland/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hyerimland.hellospring.repository;
+
+import hyerimland.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
