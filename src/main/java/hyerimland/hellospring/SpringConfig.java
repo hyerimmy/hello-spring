d6f695f629ba8bdf83da7d94104860a81d0aef0f
hyerimmy <heyrim2010@naver.com>
2022-12-11 02:41:36 +0900

    순수 JDBC

--- src/main/java/hyerimland/hellospring/SpringConfig.java
+++ src/main/java/hyerimland/hellospring/SpringConfig.java
@@ -1,19 +1,32 @@
 package hyerimland.hellospring;
 
+import hyerimland.hellospring.repository.JdbcMemberRepository;
 import hyerimland.hellospring.repository.MemberRepository;
 import hyerimland.hellospring.repository.MemoryMemberRepository;
 import hyerimland.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
